0f5e1b5fc4efb847baf4997022354e35a8a31609
Moises Alvarez <mfalvare@espol.edu.ec>
2022-11-13 06:08:22 -0500

    Agregando el menu Vendedor

--- src/main/java/com/alvarez/concesionariadeautos/ConcesionariaDeAutos.java
+++ src/main/java/com/alvarez/concesionariadeautos/ConcesionariaDeAutos.java
@@ -59,7 +59,7 @@ public class ConcesionariaDeAutos {
                             System.out.println("--------------------------------------");
                             System.out.println("\tBienvenido Vendedor");
                             System.out.println("--------------------------------------");
-//                            menuVendedor();
+                            menuVendedor();
                             break;
                         case "JefeDeTaller":
                             System.out.println("Credenciales correctas");
@@ -180,9 +180,11 @@ public class ConcesionariaDeAutos {
                                     System.out.println("Ya ha cotizado este vehiculo anteriormente, elija  otro vehiculo");
                                 } else {
                                     Solicitud solicitud = clienteLogeado.cotizarVehiculo(vehiculoInteresado);
-                                    getVendedorAleatorio().addSolicitud(solicitud);//En esta linea le agregamos a un vendedor aleatorio la  cotizacion que genero el 
+                                    Vendedor vendedorAleatorio = getVendedorAleatorio();
+                                    vendedorAleatorio.addSolicitud(solicitud);//En esta linea le agregamos a un vendedor aleatorio la  cotizacion que genero el 
                                     clienteLogeado.addSolicitud(solicitud);// Le agregamos la solicitud que genero el Cliente a su lista de solicitudes para que pueda verificar el estado de sus solicitudes
                                     System.out.println("Su solicitud ha sido enviada, recibira un mensaje cuando sea contestada");
+                                    System.out.println("El vendedor aleatorio: " + vendedorAleatorio.getUsuario());
                                 }
 
                             }
@@ -214,6 +216,90 @@ public class ConcesionariaDeAutos {
 
             }
 
+        }
+
+    }
+
+    public static void menuVendedor() {
+        Vendedor vendedorLogeado = (Vendedor) usuarioLogeado;
+        boolean salir = false;
+        int opciones;
+        Scanner opcionesVendedor = new Scanner(System.in);
+
+        System.out.println("Usuario: " + vendedorLogeado.getNombres() + " " + vendedorLogeado.getApellidos());
+
+        while (!salir) {
+            System.out.println("\tOpciones:");
+            System.out.println("1) Consultar Vehiculos");
+            System.out.println("2) Consultar Solicitudes de Cotizacion");
+            System.out.println("3) Salir");
+            opciones = opcionesVendedor.nextInt();
+            switch (opciones) {
+                case 1:
+                    int cont = 1;
+                    for (Vehiculo vehiculo : vehiculos) {
+                        System.out.println(cont + ") " + vehiculo.mostrarInformacion());
+                        cont++;
+
+                    }
+                    break;
+                case 2:
+                    if(vendedorLogeado.getSolicitudes().size()==0){
+                        System.out.println("No tiene nuevas solicitudes de Cotizacion");
+                        break;                   
+                    }
+
+                    for (Solicitud solicitud : vendedorLogeado.getSolicitudes()) {
+                        System.out.println(solicitud.mostrarInformacion());
+
+                    }
+                    System.out.println("1) Aprobar Solicitud");
+                    System.out.println("2) Rechazar Solicitud");
+                    System.out.println("3) Cancelar");
+                    System.out.println("Seleccione una opcion:");
+                    opciones = opcionesVendedor.nextInt();
+                    switch (opciones) {
+                        case 1:
+                            cont = 1;
+                            for (Solicitud s : vendedorLogeado.getSolicitudes()) {
+                                System.out.println(cont + ") " + s.mostrarInformacion());
+                                cont++;
+                            }
+                            System.out.println("Seleccione la opcion que desea Aprobar");
+                            int opcionAprobada = opcionesVendedor.nextInt();
+                            Solicitud solicitud = vendedorLogeado.getSolicitudes().get(opcionAprobada-1);
+                            solicitud.setEstado(EstadoSolicitud.APROBADA);
+                            vendedorLogeado.enviarCotizacion((Cliente) solicitud.getUsuario(), solicitud.getVehiculo());
+                            System.out.println("Cotizacion Enviada");
+                            break;
+                        case 2:
+                            cont = 1;
+                            for (Solicitud s : vendedorLogeado.getSolicitudes()) {
+                                System.out.println(cont + ") " + s.mostrarInformacion());
+                                cont++;
+                            }
+                            System.out.println("Seleccione la opcion que desea Aprobar");
+                            int opcionRechazada = opcionesVendedor.nextInt();
+                            Solicitud solicitudRechazada = vendedorLogeado.getSolicitudes().get(opcionRechazada-1);
+                            solicitudRechazada.setEstado(EstadoSolicitud.RECHAZADA);
+                            System.out.println("Ingrese motivo de rechazo a la solicitud: ");
+                            Scanner motivoEntrada = new Scanner(System.in);
+                            String motivo = motivoEntrada.next();
+                            vendedorLogeado.rechazarCotizacion((Cliente) solicitudRechazada.getUsuario(), motivo);
+                            System.out.println("Cotizacion Rechazada");
+                            vendedorLogeado.eliminarSolicitud(solicitudRechazada);
+                            motivoEntrada.close();
+
+                            break;
+                        case 3:
+                            break;
+                            
+                    }
+                case 3:
+                    salir= true;
+
+                    break;
+            }
         }
 
     }
